55dd9dfbe375a3192113164f3f6043400ceebd04
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-02-25 18:52:08 +0600

    createPost function created and getPosts function changed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,10 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 
+import PostMessage from "../models/postMessage.js";
+
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
-    res.send('THIS WORKS!');
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPostMessage = new PostMessage(post);
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
 
 export default router;
