0666b48302edee614aeb38a44ef9867d0bfd9caa
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-02-25 15:17:09 +0600

    index.js is connecting to the database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://it-superninja:it-superninja123@cluster0.ygv653n.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT|| 5000;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
