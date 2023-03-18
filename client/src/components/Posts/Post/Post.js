0dc9f6846c021ee17f32b08cef3369dd1388234e
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-03-18 19:55:21 +0600

    react-reouter-dom installed, index.css changed, getPost, getPosts and createPost changed, Post and Posts components changed

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
+import { useDispatch } from "react-redux";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
 
 import useStyles from "./styles.js";
 
-const Post = () => {
+const Post = ({ post }) => {
+    const navigate = useNavigate();
     const classes = useStyles();
+
+    const openPost = (e) => {
+        // dispatch(getPost(post._id, history));
+    
+        navigate(`/posts/${post._id}`);
+      };
     
     return (
-        <h1>Post</h1>
+        <Card className={classes.card}>
+            <ButtonBase
+                component="span"
+                name="test"
+                className={classes.cardAction}
+                onClick={openPost}
+            >
+                <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
+                <div className={classes.overlay}>
+                    <Typography variant="h6" >{post.creator}</Typography>
+                    <Typography variant="body2" >{moment(post.createdAt).fromNow()}</Typography>
+                </div>
+                <div className={classes.overlay2} name="edit">
+                    <Button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                        style={{ color: 'white' }}
+                        size="small"
+                    >
+                        <MoreHorizIcon fontSize="medium" />
+                    </Button>
+                </div>
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2" >{post.tags.map((tag) => `#${tag} `)}</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 20).join(' ')}...</Typography>
+                </CardContent>
+            </ButtonBase>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="secondary" onClick={() => {}}>
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
@@ -6,14 +7,17 @@ import thunk from "redux-thunk";
 
 import { reducers } from "./reducers";
 import App from "./App.js";
+import "./index.css";
 
 const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.Fragment>
 );
